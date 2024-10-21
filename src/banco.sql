225d236d5cf1a2d7fdb2d4f1f5e4e279d16a0b0a
GuilhermeTheGrego <118355850+GuilhermeTheGrego@users.noreply.github.com>
2024-10-21 08:22:43 -0400

    Mudei hj

--- src/banco.sql
+++ src/banco.sql
@@ -8,6 +8,15 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
+create table usuarios(
+id int primary key,
+nome varchar(200),
+email varchar(100),
+create_at timestamp,
+update_at timestamp
+);
 
 INSERT INTO produtos VALUES
 (1,"Iphone","Celular RUIM",5000.50,"SEM IMAGEM");
+insert into usuarios VALUES
+();

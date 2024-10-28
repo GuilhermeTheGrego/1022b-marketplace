34ea558ce4a07b84a872856df1cea5f09961e033
GuilhermeTheGrego <118355850+GuilhermeTheGrego@users.noreply.github.com>
2024-10-28 07:15:24 -0400

    atualizamos

--- banco.sql
+++ banco.sql
@@ -1,27 +1,23 @@
 DROP DATABASE IF EXISTS defaultdb;
 CREATE DATABASE defaultdb;
-USE defaultdb;
+
 
 CREATE TABLE IF NOT EXISTS produtos (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
     descricao VARCHAR(50),
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
-
-/*Criamos uma tabela para usuarios*/
+INSERT INTO produtos VALUES
+(1,"Iphone", "Celular RUIM", 5000.50, "SEM IMAGEM");
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(200) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     created_at TIMESTAMP,
-    updated_at TIMESTAMP 
+    updated_at TIMESTAMP
 );
-
-INSERT INTO produtos (nome, descricao, preco, imagem) VALUES
-("Iphone", "Celular RUIM", 5000.50, "SEM IMAGEM");
-
--- Exemplo de inserção de usuários
+USE defaultdb;
 INSERT INTO usuarios VALUES
 (1,'guilherme','gui.souza11@gmail.com','2024-10-23 10:00:00','2024-11-22 10:00:00')

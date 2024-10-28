e824ca92c470f83316f2e8bffbecc3e167d30a61
GuilhermeTheGrego <118355850+GuilhermeTheGrego@users.noreply.github.com>
2024-10-28 08:22:43 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,26 @@ app.get("/produtos", async (req, res) => {
         res.status(500).send("Erro do servidor ao buscar produtos");
     }
 });
+app.post("/produtos", async (req, res) => {
+    try {
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022b",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        });
+        const {id, nome,descricao, preco, imagem} = req.body
+        const [result,fields] =
+        await conexao.query("insert into produtos values(?,?,?,?,?)",[id,nome,descricao,preco,imagem]);
+        await conexao.end();
+
+        res.send(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erro do servidor ao buscar produtos");
+    }
+});
 
 //Criamos uma rota para listar usuário e criar a conexão com banco
 app.get("/usuarios", async (req, res) => {
@@ -44,7 +64,7 @@ app.get("/usuarios", async (req, res) => {
 
         res.send(result);
     } catch (e) {
-        console.error(e);
+        console.log(e);
         res.status(500).send("Erro do servidor ao buscar usuários");
     }
 });

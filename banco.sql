c99dc9aaf9bd361bb6243f5a954bd4dbbda32702
GuilhermeTheGrego <118355850+GuilhermeTheGrego@users.noreply.github.com>
2024-11-05 07:16:01 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -3,7 +3,7 @@ CREATE DATABASE defaultdb;
 USE defaultdb;
 
 CREATE TABLE IF NOT EXISTS produtos (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     nome VARCHAR(50),
     descricao VARCHAR(50),
     preco DECIMAL(10,2),
@@ -11,14 +11,14 @@ CREATE TABLE IF NOT EXISTS produtos (
 );
 
 CREATE TABLE IF NOT EXISTS usuarios (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(200) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    id INT PRIMARY KEY ,
+    nome VARCHAR(200) ,
+    email VARCHAR(100) ,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 INSERT INTO produtos VALUES
-(1,"Iphone", "Celular RUIM", 5000.50, "https://http2.mlstatic.com/D_Q_NP_896424-MLA71783367608_092023-O.webp");
+(1,"Iphone", "Celular Bom", 5000.50, "https://http2.mlstatic.com/D_Q_NP_896424-MLA71783367608_092023-O.webp");
 INSERT INTO usuarios VALUES
 (1,'guilherme','gui.souza11@gmail.com','2024-10-23 10:00:00','2024-11-22 10:00:00');

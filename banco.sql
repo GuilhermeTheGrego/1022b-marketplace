3220d8354db52e173a3a10bb46bc702fdd9d5b6d
GuilhermeTheGrego <118355850+GuilhermeTheGrego@users.noreply.github.com>
2024-11-05 07:46:45 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -18,7 +18,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
     updated_at TIMESTAMP
 );
 
-INSERT INTO produtos VALUES
-(1,"Iphone", "Celular Bom", 5000.50, "https://http2.mlstatic.com/D_Q_NP_896424-MLA71783367608_092023-O.webp");
+INSERT INTO produtos VALUES (1, 'Iphone', 'Celular Bom', 5000.50, '');
 INSERT INTO usuarios VALUES
 (1,'guilherme','gui.souza11@gmail.com','2024-10-23 10:00:00','2024-11-22 10:00:00');
